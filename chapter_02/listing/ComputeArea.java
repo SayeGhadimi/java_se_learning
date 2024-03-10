1696efddf173220d49f2d50406f6c0334b3db342
saye.ghadimi <saye.ghadimi79@gmail.com>
2024-03-10 15:48:09 +0300

    listing 2.1

---
+++ chapter_02/listing/ComputeArea.java
@@ -0,0 +1,15 @@
+public class ComputeArea {
+	public static void main(String[] args) {
+		double radius; // Declare radius
+		double area; // Declare area
+		// Assign a radius
+
+		radius = 20; // radius is now 20
+		area = radius * radius * 3.14159;
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+
+		
+		
+	}
+	
+}
