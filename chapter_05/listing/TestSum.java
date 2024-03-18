88f9855aa42b9ee6a91631fb93a5704e7ac27fa1
saye.ghadimi <saye.ghadimi79@gmail.com>
2024-03-18 22:38:29 +0300

    listing 5.8

---
+++ chapter_05/listing/TestSum.java
@@ -0,0 +1,10 @@
+public class TestSum {
+	public static void main(String[] args) {
+		float sum = 0;
+		for (float i = 0.01f; i <= 1.0f; i = i + 0.01f)
+			sum += i;	
+
+		System.out.println("The sum is " + sum);
+
+	}
+}
