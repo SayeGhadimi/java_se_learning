24f09be737059552a80a018f0ba5e85acc9825a9
saye <saye.ghadimi79@gmali.com>
2024-05-12 15:29:03 +0300

    listing 9.9

---
+++ chapter_09/listing/TestPassObject.java
@@ -0,0 +1,25 @@
+public class TestPassObject {
+    /** Main method */
+	public static void main(String[] args) {
+		// Create a Circle object with radius 1
+		Circle myCircle =
+				new Circle(1); // Use the Circle class in Listing 9.8
+		        // Print areas for radius 1, 2, 3, 4, and 5.
+		        int n = 5;
+		        printAreas(myCircle, n);
+
+		        // See myCircle.radius and times
+		        System.out.println("\n" + "Radius is " + myCircle.getRadius());
+		        System.out.println("n is " + n);
+	}
+
+		       /** Print a table of areas for radius */
+		       public static void printAreas(Circle c, int times) {
+		       System.out.println("Radius \t\tArea");
+		       while (times >= 1) {
+			         System.out.println(c.getRadius() + "\t\t" + c.getArea());
+			         c.setRadius(c.getRadius() + 1);
+			         times--;
+			   }
+		}
+  }
