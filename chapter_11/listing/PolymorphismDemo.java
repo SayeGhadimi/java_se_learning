b469eda309015d8f8e634553cdc2bcbcc694ab5d
saye <saye.ghadimi79@gmali.com>
2024-05-20 21:05:26 +0300

    listing 11.5

---
+++ chapter_11/listing/PolymorphismDemo.java
@@ -0,0 +1,17 @@
+public class PolymorphismDemo {
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        // Display circle and rectangle properties
+        displayObject(new Circle(1, "red", false));
+        displayObject(new Rectangle(1, 1, "black", true));
+    }
+
+    /**
+     * Display geometric object properties      */
+    public static void displayObject(GeometricObject object) {
+        System.out.println("Created on " + object.getDateCreated() + ". Color is " + object.getColor());
+
+    }
+}
