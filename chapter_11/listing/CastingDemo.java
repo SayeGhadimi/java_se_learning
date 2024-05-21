46b81948b5a7e783ae2bf088cbbae492d16488f2
saye <saye.ghadimi79@gmali.com>
2024-05-21 20:50:52 +0300

    listing 11.7

---
+++ chapter_11/listing/CastingDemo.java
@@ -0,0 +1,22 @@
+public class CastingDemo {
+    /** Main method */
+    public static void main(String[] args) {
+    // Create and initialize two objects
+        Object object1 = new Circle(1);
+        Object object2 = new Rectangle(1, 1);
+
+
+    // Display circle and rectangle
+
+    }
+    /** A method for displaying an object */
+    public static void displayObject(Object object) {
+        if (object instanceof Circle) {
+            System.out.println("The circle area is " + ((Circle)object).getArea());
+            System.out.println("The circle diameter is " + ((Circle)object).getDiameter());
+        }
+                  else if (object instanceof Rectangle) {
+                      System.out.println("The rectangle area is " + ((Rectangle)object).getArea());
+                  }
+      }
+}
