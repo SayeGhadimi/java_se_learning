f0a24465f574577eb3f960d9ec365ff3037110d7
saye <saye.ghadimi79@gmali.com>
2024-06-08 16:51:50 +0300

    listing 19.5

---
+++ chapter_19/listing/Max.java
@@ -0,0 +1,11 @@
+public class Max {
+    /** Rerurn the maximum of two objects */
+    public static Comparable max (comparable o1 ,Comparable o2) {
+        if (o1.compareTo(o2) > 0)
+            return o1 ;
+        else
+            return o2 ;
+
+    }
+         
+}
